361ee5046d339a9f5fc9788e5d6758241e227b07
basmasabkhi <sabkhi.basma89@gmail.com>
2024-07-07 18:35:26 +0200

    version67

--- src/components/espaceCoach/Articles.js
+++ src/components/espaceCoach/Articles.js
@@ -11,19 +11,21 @@ import logo from "../../images/logo.jpg";
 import { GrLinkedin } from "react-icons/gr";
 import { FaFacebook } from "react-icons/fa";
 import { MdPerson } from "react-icons/md";
+import { Dialog, DialogContent, IconButton } from "@mui/material";
+import CloseIcon from "@mui/icons-material/Close";
 
 const Articles = () => {
   const dispatch = useDispatch();
   const { Articles } = useSelector((state) => state.article);
   const [currentPage, setCurrentPage] = useState(1);
-  const [showModal, setShowModal] = useState(false); // État pour afficher/cacher le modal
-  const [selectedArticle, setSelectedArticle] = useState(null); // État pour stocker l'article sélectionné
+  const [selectedArticle, setSelectedArticle] = useState(null);
+  const [showModal, setShowModal] = useState(false);
 
   useEffect(() => {
     dispatch(GetArticle());
   }, [dispatch]);
 
-  const pageSize = 6; // Nombre d'articles par page
+  const pageSize = 6;
 
   const onPageChange = (page) => {
     setCurrentPage(page);
@@ -32,13 +34,11 @@ const Articles = () => {
   const startIndex = (currentPage - 1) * pageSize;
   const endIndex = Math.min(startIndex + pageSize, Articles.length);
 
-  // Fonction pour ouvrir le modal et définir l'article sélectionné
   const openModal = (article) => {
     setSelectedArticle(article);
     setShowModal(true);
   };
 
-  // Fonction pour fermer le modal
   const closeModal = () => {
     setShowModal(false);
   };
@@ -56,30 +56,30 @@ const Articles = () => {
           overflow: "hidden",
         }}
       >
-        <div >
+        <div>
           <h3 className="ArtcTitre">Articles</h3>
         </div>
       </div>
-
-      <div className="article-list">
-        {Articles.slice(startIndex, endIndex).map((article, index) => (
-          <div key={index} className="article-card" onClick={() => openModal(article)}>
-            <Card>
-              <Card.Img variant="top" src={getImageUrl(article.photo)} height="250px" />
-              <Card.Body>
-                <Card.Title className="article-card-title">{article.titre}</Card.Title>
-                <Card.Text>
-                  <div className="article-card-author">
-                    <MdPerson className="article-card-author-icon" />
-                    <h5>{article.auteur}</h5>
-                  </div>
-                </Card.Text>
-              </Card.Body>
-            </Card>
-          </div>
-        ))}
+      <div className="Article-Containe">
+        <div className="article-list">
+          {Articles.slice(startIndex, endIndex).map((article, index) => (
+            <div key={index} className="article-card" onClick={() => openModal(article)}>
+              <Card>
+                <Card.Img variant="top" src={getImageUrl(article.photo)} width="100%" height="300px" />
+                <Card.Body>
+                  <Card.Title className="article-card-title">{article.titre}</Card.Title>
+                  <Card.Text>
+                    <div style={{ display: "flex" }}>
+                      <MdPerson className="article-card-author-icon" />
+                      <h3 className="article-card-author">{article.auteur}</h3>
+                    </div>
+                  </Card.Text>
+                </Card.Body>
+              </Card>
+            </div>
+          ))}
+        </div>
       </div>
-
       <div style={{ textAlign: "center", marginTop: "20px" }}>
         {Array.from({ length: Math.ceil(Articles.length / pageSize) }).map((_, index) => (
           <button
@@ -100,44 +100,43 @@ const Articles = () => {
           </button>
         ))}
       </div>
-
-      {/* Modal pour afficher les détails de l'article sélectionné */}
-      {showModal && selectedArticle && (
-        <div className="modalBaghroundArticle">
-          <span className="close" onClick={closeModal}>&times;</span>
-          <div className="modalcontainerArticle">
-            <img src={logo} alt="logo" width="220px" height="70" />
-            <hr />
-            <div>
-              <img src={getImageUrl(selectedArticle.photo)} alt="Article" className="Artc-image" width="100px" />
-              <div className="modal-Artc">
-              <h3>{selectedArticle.titre}</h3>
-              <div dangerouslySetInnerHTML={{ __html: selectedArticle.texte }} />
-                <div className="Artc-info">
-              
-                
-              
-                  <div className="Artc-author">
-                <div  style={{display:'flex',alignItems:'center'}}>
-                <MdPerson className="artic-author-icon"  />
-                <h5 className="articl-auteur">{selectedArticle.auteur} </h5>
-                </div>
+      <Dialog open={showModal} onClose={closeModal} fullWidth maxWidth="md">
+        <DialogContent style={{ padding: '20px', position: 'relative' }}>
+          <IconButton
+            style={{ position: 'absolute', top: '10px', right: '10px' }}
+            onClick={closeModal}
+          >
+            <CloseIcon />
+          </IconButton>
+          {selectedArticle && (
+            <div className="ArticleContai">
+              <img src={logo} alt="Article" style={{ width: '180px' }} />
+              <hr />
+              <img src={getImageUrl(selectedArticle.photo)} alt="Article" style={{ display: "block", margin: "0 auto", width: '200px' }} />
+              <h3 className="ART-titre">{selectedArticle.titre}</h3>
+              <div className="Art-Descrip" dangerouslySetInnerHTML={{ __html: selectedArticle.texte }} />
+              <div className="Artc-inf">
+                <div className="Artc-author">
+                  <div style={{ paddingLeft: '30px' }}>
+                    <div style={{ display: 'flex', alignItems: 'center' }}>
+                      <MdPerson className="artic-author-icon" />
+                      <h5 className="articl-auteur">{selectedArticle.auteur}</h5>
+                    </div>
                     <div className='partageArticle'>
-              <button className="linkedin-buttonArt">
-                <GrLinkedin /> Partage
-              </button>
-              <button className="facebook-buttonArt">
-                <FaFacebook /> Partage
-              </button>
-            </div> 
+                      <button className="linkedin-buttonArt">
+                        <GrLinkedin /> Partage
+                      </button>
+                      <button className="facebook-buttonArt">
+                        <FaFacebook /> Partage
+                      </button>
+                    </div>
                   </div>
                 </div>
               </div>
             </div>
-          </div>
-        </div>
-      )}
-
+          )}
+        </DialogContent>
+      </Dialog>
       <Newsletter />
       <Footer />
     </>
